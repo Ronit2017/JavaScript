5bf19275585d1970c4b01e4b42feaa1d903ce00e
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 08:43:00 +0000

    date and time

---
+++ basicjs/dateTime.js
@@ -0,0 +1,10 @@
+let myDate  = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+let myCreateDate = new Date(2023,9,5)
+console.log(myCreateDate.toDateString());
+
+let myTime = Date.now()
+console.log(myTime);
