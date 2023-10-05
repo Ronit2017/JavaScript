5746b3ec41ad554cb2b59427aec88390074f9704
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 09:04:36 +0000

    Array

---
+++ basicjs/Array.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,5,7,8]
+const arr1 = [11,22,23,5,27,28]
+console.log(arr);
+const arr3 = arr.concat(arr1)
+console.log(arr3);
+// console.log(array.isArray("honey"));
+// console.log(array.from("honey"));
+// console.log(arr3);
+// arr.push(6)
+// arr.pop()
+
+// arr.unshift(9)
+
+// console.log(arr);
+const newArr = arr.join()
+console.log(newArr);
