a6576b535994e6d32dd103fdf99654b2febe3b49
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 07:00:15 +0000

    conversion

---
+++ basicjs/conversion.js
@@ -0,0 +1,11 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33abc"=>NaN
+// true =>1; false=>0;
+//"33"=>33
