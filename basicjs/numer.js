a2022d5157abfc453575eb2cd5dc9647dd095c55
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 08:27:54 +0000

    number-maths

---
+++ basicjs/numer.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+
+const rrr = new Number(500)
+console.log(rrr);
+console.log(rrr.toFixed(2));
+console.log(rrr.toString().length);
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//*********math*******************/
+console.log(Math);
+console.log(Math.random());
+console.log(Math.min(4,5,3,2));
+console.log(Math.max(4,5,3,2));
+console.log(Math.round(4.6));
+console.log(Math.abs(4));
