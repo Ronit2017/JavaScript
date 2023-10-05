fd4480d62a1e3d13d1c15034d64b71458de08a61
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 06:47:00 +0000

    dataTpes

---
+++ basicjs/dataTypes.js
@@ -0,0 +1,16 @@
+let name = "honey"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string=>""
+// boolean=>true/false
+//null=>standalone value
+//undefined=> # let state; that mean value is not define
+// symbol=>unique
+
+//object
+
+console.log(typeof"honey");
+
