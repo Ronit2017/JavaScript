f4fb4a29ab2e7dfa851ab60a3dd62e84b770b92f
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 07:31:32 +0000

    dt_summary

---
+++ basicjs/dt_summary.js
@@ -0,0 +1,18 @@
+// Primitive
+
+// 7 types : String, number boolean ,null,undefined ,symbol,bigInt
+
+const igNumber = 35353553622727n
+
+//Reference
+
+//Array,Objects,Functions
+
+const arrr = ["honey","ronit"]
+let myobj= {
+    name:"honey",
+    age: 20,
+}
+const myFunction = function(){
+    console.log("hello");
+}
