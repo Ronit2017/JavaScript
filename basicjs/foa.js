eca1b43888cfbf1cfecc7061ad938418e73817c8
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 16:09:30 +0000

    function 3.o

---
+++ basicjs/foa.js
@@ -0,0 +1,14 @@
+function calculate(...num1){
+    return num1
+}
+console.log(calculate(200,400,500));
+
+const user =  {
+    username:"honey",
+    price:200
+}
+function object(anyobject){
+    console.log(`username is${anyobject.username} any price is${anyobject.price}`);
+}
+
+object(user)
