1e87af2945e06501c5d941f9df3c365c2f7adee6
Ronit2017 <144896429+Ronit2017@users.noreply.github.com>
2023-10-05 05:43:50 +0000

    variables

---
+++ basicjs/variables.js
@@ -0,0 +1,7 @@
+const accountId = 2017
+let accountEmail = "ronitchauhan@gmail.com"
+var accountPassword = "12345"
+
+// accountId = 12
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword]);
